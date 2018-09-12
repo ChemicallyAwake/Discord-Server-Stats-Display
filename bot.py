85044f161ba3df27f58e20b747c4e8230ff74db6
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-09-12 03:09:00 -0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+import format
 
 bot = commands.Bot(command_prefix='Hello Friend')
 bot.remove_command('help') # remove that help command
@@ -20,13 +21,13 @@ async def update_count(count):
     all, online, robot = count
     
     ch_all = bot.get_channel('489143518716100629')
-    name_all = "ꓮLL 𐌑EMBERS፡ " + str(all)
+    name_all = await format.convert_string('Total : ') + str(all)
 
     ch_online = bot.get_channel('489146748720119818')
-    name_online = "ꓳNLINE 𐌑EMBERS፡ " + str(online-robot)
+    name_online = await format.convert_string('Online : ') + str(online-robot)
 
     ch_robot = bot.get_channel('489146790482935808')
-    name_robot = "𝖡OTS፡ " + str(robot)
+    name_robot = await format.convert_string('Bot : ') + str(robot)
 
     await bot.edit_channel(channel=ch_all, name=name_all)
     await bot.edit_channel(channel=ch_online, name=name_online)
