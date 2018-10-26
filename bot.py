c1da3330a69cca3c0888cff585d72a80ef166489
ChemicallyAwake <41243150+ChemicallyAwake@users.noreply.github.com>
2018-10-26 13:56:11 -0700

    The channel id's are no longer hard coded

--- bot.py
+++ bot.py
@@ -3,35 +3,30 @@ from discord.ext import commands
 import os
 import format
 
-bot = commands.Bot(command_prefix='Hello Friend')
-bot.remove_command('help') # remove that help command
+bot = commands.Bot(command_prefix='!@#$%^&*()')
+bot.remove_command('help')
 
 async def get_server_count():
-    all=online=robot=0  # set vars to zero
+    all=online=robot=0
     for member in bot.get_all_members():
-        all += 1
-        if member.status != discord.Status.offline:
-            online += 1
         if member.bot:
-            robot += 1 
+            robot += 1
+        else:
+            all += 1
+            if member.status != discord.Status.offline:
+                online += 1 
         
     return all,online,robot
 
 async def update_count(count):
     all, online, robot = count
     
-    ch_all = bot.get_channel('489143518716100629')
-    name_all = await format.convert_string('Total : ') + str(all-robot)
-
-    ch_online = bot.get_channel('489146748720119818')
-    name_online = await format.convert_string('Online : ') + str(online-robot)
-
-    ch_robot = bot.get_channel('489146790482935808')
-    name_robot = await format.convert_string('Bots : ') + str(robot)
-
-    await bot.edit_channel(channel=ch_all, name=name_all)
-    await bot.edit_channel(channel=ch_online, name=name_online)
-    await bot.edit_channel(channel=ch_robot, name=name_robot)
+    await bot.edit_channel(channel=bot.get_channel(os.getenv('TOTAL')),
+                           name= await format.convert_string('Total : ') + str(all))
+    await bot.edit_channel(channel=bot.get_channel(os.getenv('ONLINE')),
+                           name=await format.convert_string('Online : ') + str(online))
+    await bot.edit_channel(channel=bot.get_channel(os.getenv('BOTS')),
+                           name=await format.convert_string('Bots : ') + str(robot))
 
 @bot.event
 async def on_member_join(member):
